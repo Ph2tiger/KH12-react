042a28fa1d7d00d9448d884bc3cb62c6425f9e47
ph2tiger <ph2tiger@naver.com>
2023-11-06 17:49:00 +0900

    도서 삭제 구현

--- demo05/src/components/Book.js
+++ demo05/src/components/Book.js
@@ -1,26 +1,47 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {FaHighlighter, FaTrashCan} from "react-icons/fa6";
+import { FaPenToSquare, FaTrashCan } from "react-icons/fa6";
 
 import "./Book.css"
 
-const Book = (porps)=>{
+const Book = (porps) => {
   const [bookList, setBookList] = useState([]);
+
+    const loadBook = ()=>{
+        //서버에 있는 도서 정보를 불러와서 state에 반영하는 코드
+        axios({
+            url:"http://localhost:8080/book/",
+            method:"get"
+        })
+        .then(response=>{
+            setBookList(response.data);
+        })
+        .catch(err=>{
+            window.alert("통신 오류 발생");
+        });
+    };
+
   useEffect(()=>{
-    //서버에 있는 도서 정보를 불러와서 state에 반영하는 코드
-    axios({
-      url: "http://localhost:8080/book/",
-      method: "get"
-    })
-    .then(respons => {
-      setBookList(respons.data)
-    })
-    .catch(err => {
-      window.alert("통신 오류 발생")
-    });
+    loadBook();
   }, []);
 
-  return(
+  //도서 삭제
+  const deleteBook = (book) => {
+    const choice = window.confirm("정말 삭제하시겠습니까?");
+    if (choice === false) return;
+
+    axios({
+      //url: "http://localhost:8080/book/"+book.bookId,
+      url: `http://localhost:8080/book/${book.bookId}`,
+      method: "delete"
+    })
+      .then(response => {
+        loadBook();//목록 갱신
+      })
+      .catch(err => { })
+  };
+
+  return (
     <>
       <div className="row">
         <div className="col">
@@ -40,7 +61,7 @@ const Book = (porps)=>{
               <th>가격</th>
               <th className="pc-only">출판일</th>
               <th className="pc-only">페이지 수</th>
-              <th>출판사</th>
+              <th className="pc-only">출판사</th>
               <th>관리</th>
             </thead>
             <tbody>
@@ -53,9 +74,12 @@ const Book = (porps)=>{
                   <td>{book.bookPrice}</td>
                   <td className="pc-only">{book.bookPublicationDate}</td>
                   <td className="pc-only">{book.bookPageCount}</td>
-                  <td>{book.bookPublisher}</td>
+                  <td className="pc-only">{book.bookPublisher}</td>
                   <td>
-                    <FaHighlighter/><FaTrashCan/>
+                    {/* 아이콘 자리 */}
+                    <FaPenToSquare className="text-warning" />
+                    <FaTrashCan className="text-danger"
+                      onClick={e => deleteBook(book)} />
                   </td>
                 </tr>
               ))}
