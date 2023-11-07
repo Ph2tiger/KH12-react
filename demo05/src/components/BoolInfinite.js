ce038ee7e678145a58091f8f0b71f8f65a1eb038
ph2tiger <ph2tiger@naver.com>
2023-11-07 17:21:22 +0900

    무한 스크롤 구현

--- demo05/src/components/BoolInfinite.js
+++ demo05/src/components/BoolInfinite.js
@@ -1,39 +1,98 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import throttle from "lodash/throttle";//특정 함수의 발생주기를 설정(강제 성능 하향)
+//import debounce from "lodash/debounce";//특정 이벤트의 마지막만 실행하도록 설정
 
 const BookInfinite = (props) => {
   const [page, setPage] = useState(1);
   const [size, setSize] = useState(30);
   const [bookList, setBookList] = useState([]);
 
-  const loadBook =()=>{
+  //ref를 이용해서 flag변수를 생성(화면과는 무관)
+  const loading = useRef(false);
+  const loadBook = () => {
+    loading.current=true;//로딩중으로 설정(순서 보장)
     axios({
-      url:`${process.env.REACT_APP_REST_API_URL}/book/page/${page}/size/${size}`,
-      method:"get"
+      url: `${process.env.REACT_APP_REST_API_URL}/book/page/${page}/size/${size}`,
+      method: "get"
     })
-    .then(response=>{
-      //setBookList(response.data)//덮어쓰기
-      setBookList([...bookList, ...response.data]);//spread연산자
-      //setBookList(bookList.con)
-    });
+      .then(response => {
+        //setBookList(response.data)//덮어쓰기
+        setBookList([...bookList, ...response.data]);//spread연산자
+        loading.current = false;//로딩완료로 설정(순서 보장)
+        //setBookList(bookList.con)
+      });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     loadBook();
   }, [page]);
 
   //다음페이지
-  const nextPage = ()=>{
-    setPage(page+1);//페이지 1증가
+  const nextPage = () => {
+    setPage(page + 1);//페이지 1증가
     loadBook();
   };
 
   //개수가 변하면 페이지를 1로, 목록을 모두 지우고 다시 불러와야 한다
-  useEffect(()=>{
+  useEffect(() => {
     setPage(1);
     setBookList([]);
     //loadBook();
   }, [size]);
+
+
+  ////////////////////////////////////////////////////////////////
+  // 스크롤 이맨트 처리(scroll event handing)
+  //- 화면이 실행되면 window에 scroll 이벤트 설정
+  //- 화면이 해제되기 직전에 이밴트 해제
+  ////////////////////////////////////////////////////////////////
+  useEffect(()=>{
+    if(loading.current === true) {//로딩이 진행중인 상황
+        return;//그만해
+    }
+
+    //화면 생성 시 할 작업
+    const listener = throttle(()=>{
+        //console.log("스크롤이 굴러간다!");
+        const percent = calculateScrollPercentage();
+        console.log("퍼센트 = " + percent);
+        if(percent >= 60) {
+            nextPage();
+        }
+    }, 750);
+    window.addEventListener("scroll", listener);
+    console.log("걸렸다");
+
+    //화면 해제 시 할 작업
+    return ()=>{
+        window.removeEventListener("scroll", listener);
+        console.log("풀렸다");
+    };
+});
+
+//스크롤 퍼센트 구하는 함수(Feat.GPT)
+const calculateScrollPercentage = ()=>{
+    // 현재 스크롤 위치
+    var scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+  
+    // 문서의 전체 높이
+    var documentHeight = Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    );
+  
+    // 브라우저의 뷰포트 높이
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  
+    // 스크롤 퍼센트 계산
+    var scrollPercentage = (scrollPosition / (documentHeight - windowHeight)) * 100;
+  
+    return scrollPercentage;
+  }
 
   return (
     <>
@@ -45,7 +104,7 @@ const BookInfinite = (props) => {
 
       <div className="row mt-4">
         <div className="col-2 offset-10">
-          <select value={size} onChange={e=>setSize(e.target.value)}>
+          <select value={size} onChange={e => setSize(e.target.value)}>
             <option value={20}>20개씩 보기</option>
             <option value={30}>30개씩 보기</option>
             <option value={50}>50개씩 보기</option>
@@ -66,13 +125,13 @@ const BookInfinite = (props) => {
               </tr>
             </thead>
             <tbody>
-              {bookList.map(book=>(
-              <tr key={book.bookId}>
-                <td>{book.bookId}</td>
-                <td>{book.bookTitle}</td>
-                <td>{book.bookAuthor}</td>
-                <td>{book.bookPublisher}</td>
-              </tr>  
+              {bookList.map(book => (
+                <tr key={book.bookId}>
+                  <td>{book.bookId}</td>
+                  <td>{book.bookTitle}</td>
+                  <td>{book.bookAuthor}</td>
+                  <td>{book.bookPublisher}</td>
+                </tr>
               ))}
             </tbody>
           </table>
